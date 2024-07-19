0a61e942d0b47c0cd9ad16fef9bf9c7588bf6f9c
Malcom X <Taajlogan@gmail.com>
2024-07-19 13:10:41 -0400

    rendering gallery page and dummy seed data

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Post from "../components/Post";
 import Headerhome from "../components/Headerhome";
 import Footer from "../components/Footer";
+import Gallery from "../components/Gallery"
 const Home = () => {
     return (
         <>
             <Headerhome />
             <Post />
+            <Gallery/>
             <Footer />
             </>
     );
