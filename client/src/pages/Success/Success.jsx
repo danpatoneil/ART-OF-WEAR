9d7a704789839f3f0199f673346e36fd304e07f2
danpatoneil <danpatoneil@gmail.com>
2024-07-24 16:21:56 -0400

    Update Success.jsx

--- client/src/pages/Success/Success.jsx
+++ client/src/pages/Success/Success.jsx
@@ -1,12 +1,26 @@
-import { useMutation } from "@apollo/client";
+import { useEffect } from "react";
 import "./Success.css";
-import {UPDATE_ORDER} from '../../utils/mutations'
-const Success = () => {
-    const [addOrder] = useMutation(UPDATE_ORDER)
+import { useMutation } from "@apollo/client";
+import { UPDATE_ORDER } from "../../utils/mutations";
 
-    return (
-        <p>Your order has been successfully completed!</p>
-    )
-}
+
+const Success = () => {
+  const [addOrder] = useMutation(UPDATE_ORDER);
+  useEffect(() => {
+    async function saveOrder() {
+        const searchParams = new URLSearchParams(window.location.search);
+        const orderId = searchParams.get('order_id');
+        console.log("orderID is : ", orderId)
+        const {data} = await addOrder({variables: {id: orderId, status:'Received'}})
+        console.log(data.updateOrder._id)
+    }
+    setTimeout(() => {
+      window.location.assign("/");
+    }, 3000);
+    saveOrder();
+  }, [addOrder]);
+
+  return <p>Your order was successfully processed!</p>;
+};
 
 export default Success;
