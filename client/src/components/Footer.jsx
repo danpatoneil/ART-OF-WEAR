fbe20db9044d7e10dee81c9386123a5a00c306de
danpatoneil <danpatoneil@gmail.com>
2024-07-24 18:51:33 -0400

    tweaking css for readability, tweaking gallery to link to the correct places

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,9 +4,7 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer className="footer">
-      <li>
         <Link className="new-post-button" to="/Upload">New Post</Link>
-      </li>
     </footer>
   );
 }
