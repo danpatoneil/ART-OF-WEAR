758602a02aac42288c8b2cf92d7beb202a79b094
danpatoneil <danpatoneil@gmail.com>
2024-07-24 16:19:14 -0400

    Update Gallery.jsx

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,27 +1,53 @@
-import dummydata from "../utils/image-seeds";
-import "./Gallery.css";
-// import { GET_DESIGN_LIST } from "../utils/queries";
-// import { useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import { GET_DESIGN_LIST } from "../utils/queries";
+import { useNavigate } from "react-router-dom";
 
 const GalleryPage = () => {
+  const { data, error, loading } = useQuery(GET_DESIGN_LIST);
+  const navigate = useNavigate();
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  const designs = data?.getDesigns;
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  const handleForward = () => {
+    navigate(1);
+  };
+
   return (
     <div>
       <div className="gallery-container">
-        {dummydata.map((data) => {
+        <h1>Gallery</h1>
+        {designs?.map((design) => {
           return (
-            <div className="gallery-card" key={data.id}>
-              <img src={data.url} alt={data.alt} />
+            <div className="image-card" key={design._id}>
+              <img src={design.image} alt={design._id} />
               <div className="image-id">
-                <button className = "gallery-button"
-                  onClick={() => location.href=`user/${data.id}`}
+                <button
+                  style={{
+                    backgroundColor: 'transparent',
+                    border: 'none',
+                    padding: 0,
+                    cursor: 'pointer',
+                    color: 'black'
+                  }}
+                  onClick={() => navigate(`user/${design._id}`)}
                 >
-                  {data.alt}
+                  {design._id}
                 </button>
-              </div> 
+              </div>
             </div>
           );
         })}
-        </div> 
+      </div>
+      <div className="navigation">
+        <button onClick={handleBack}>Back</button>
+        <button onClick={handleForward}>Forward</button>
+      </div>
     </div>
   );
 };
